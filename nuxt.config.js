c4694494cdd859c7d7736494fc034cf0f8b245d5
Ryo Sasaki <wor.ryosasaki@gmail.com>
2021-02-26 20:45:22 +0900

    herokuのデプロイ準備

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,5 +56,5 @@ export default {
     generate: {
         fallback: true,
     },
-    serverMiddleware: ['~/server/server'],
+    serverMiddleware: ['~/server/server', '~/server/config.json'],
 };
