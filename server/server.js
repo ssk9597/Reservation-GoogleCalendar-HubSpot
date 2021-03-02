dd20fe95898feea124f0560a61a165f74aa0faef
Ryo Sasaki <wor.ryosasaki@gmail.com>
2021-03-02 11:17:06 +0900

    herokuのデプロイ準備

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 // const config = require('./config.json')[app.get('env')];
 // console.log(config.host);
@@ -22,13 +22,13 @@ const hubSpotRoutes = require('./routes/hubSpot');
 app.use('/api', calendarRoutes);
 app.use('/api', hubSpotRoutes);
 
-app.listen(PORT, err => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('サーバーを接続しました');
-    }
-});
+// app.listen(PORT, err => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log('サーバーを接続しました');
+//     }
+// });
 
 module.exports = {
     path: '/server',
